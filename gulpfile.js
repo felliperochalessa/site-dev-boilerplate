a06d31ab94b843b989ec4e9417d161f78c457855
Fellipe Lessa <felliperochalessa@gmail.com>
2022-04-25 13:25:31 -0300

    Primeiro commit

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,9 @@ const browserSync = require('browser-sync').create()
 //compile scss into css
 function style() {
   return gulp
-    .src('./src/scss/**/*.scss')
+    .src('./src/assets/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./src/styles'))
+    .pipe(gulp.dest('./src/assets/css'))
     .pipe(browserSync.stream())
 }
 
@@ -17,9 +17,9 @@ function watch() {
       baseDir: './src'
     }
   })
-  gulp.watch('./src/scss/**/*.scss', style)
+  gulp.watch('./src/assets/scss/**/*.scss', style)
   gulp.watch('./src/*.html').on('change', browserSync.reload)
-  gulp.watch('./src/js/**/*.js').on('change', browserSync.reload)
+  gulp.watch('./src/assets/js/**/*.js').on('change', browserSync.reload)
 }
 
 exports.style = style
